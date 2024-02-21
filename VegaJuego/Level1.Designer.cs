1c465b5da3ecff424e6a79fa6c20b28b94b64d7a
AlvaroRJ01 <alvaro.romero.jimenez@ces-vegamedia.es>
2024-02-21 23:09:29 +0100

    Commit Level1

---
+++ VegaJuego/Level1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace VegaJuego
+{
+    partial class Level1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            MovEnemigos = new System.Windows.Forms.Timer(components);
+            pictureBox1 = new PictureBox();
+            labelVida = new Label();
+            pictureBox2 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            labelEscudo = new Label();
+            labelArma = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // MovEnemigos
+            // 
+            MovEnemigos.Enabled = true;
+            MovEnemigos.Tick += timer1_Tick;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImage = Properties.Resources.salud1;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Center;
+            pictureBox1.Location = new Point(760, 180);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(49, 52);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // labelVida
+            // 
+            labelVida.AutoSize = true;
+            labelVida.BackColor = Color.Transparent;
+            labelVida.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            labelVida.ForeColor = Color.Gold;
+            labelVida.Location = new Point(816, 193);
+            labelVida.Name = "labelVida";
+            labelVida.Size = new Size(46, 23);
+            labelVida.TabIndex = 1;
+            labelVida.Text = "Vida";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.BackgroundImage = Properties.Resources.escudo;
+            pictureBox2.BackgroundImageLayout = ImageLayout.Center;
+            pictureBox2.Location = new Point(760, 240);
+            pictureBox2.Margin = new Padding(3, 4, 3, 4);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(49, 52);
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.Transparent;
+            pictureBox4.BackgroundImage = Properties.Resources.Arma;
+            pictureBox4.BackgroundImageLayout = ImageLayout.Center;
+            pictureBox4.Location = new Point(760, 300);
+            pictureBox4.Margin = new Padding(3, 4, 3, 4);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(49, 52);
+            pictureBox4.TabIndex = 4;
+            pictureBox4.TabStop = false;
+            // 
+            // labelEscudo
+            // 
+            labelEscudo.AutoSize = true;
+            labelEscudo.BackColor = Color.Transparent;
+            labelEscudo.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            labelEscudo.ForeColor = Color.Gold;
+            labelEscudo.Location = new Point(816, 252);
+            labelEscudo.Name = "labelEscudo";
+            labelEscudo.Size = new Size(65, 23);
+            labelEscudo.TabIndex = 5;
+            labelEscudo.Text = "Escudo";
+            // 
+            // labelArma
+            // 
+            labelArma.AutoSize = true;
+            labelArma.BackColor = Color.Transparent;
+            labelArma.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            labelArma.ForeColor = Color.Gold;
+            labelArma.Location = new Point(816, 311);
+            labelArma.Name = "labelArma";
+            labelArma.Size = new Size(54, 23);
+            labelArma.TabIndex = 7;
+            labelArma.Text = "Arma";
+            // 
+            // Level1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.FondoFinal1;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(914, 600);
+            Controls.Add(labelArma);
+            Controls.Add(labelEscudo);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox2);
+            Controls.Add(labelVida);
+            Controls.Add(pictureBox1);
+            DoubleBuffered = true;
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Level1";
+            Text = "Form1";
+            FormClosed += Level1_FormClosed;
+            Load += Level1_Load;
+            KeyPress += Level1_KeyPress;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer MovEnemigos;
+        private PictureBox pictureBox1;
+        private Label labelVida;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox4;
+        private Label labelEscudo;
+        private Label labelArma;
+    }
+}
